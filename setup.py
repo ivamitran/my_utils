d1232147a14af752336cbd24d7005df772938ab8
Ivan Tran <ivamitran16432@gmail.com>
2024-12-23 01:46:30 -0800

    Ver 1.0.2
    Updated setup.py to reflect latest version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="my_utils",  # name of library
-    version="0.1.0",  # library version
+    version="1.0.1",  # library version
     description="A utility library for my personal use",
     author="ivamitran",
     url="https://github.com/ivamitran/my_utils",  # URL of the project
